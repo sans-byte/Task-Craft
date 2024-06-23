7ca6282a7e242b8084a12d117637609f803676c1
sans-byte <sanskarj15@gmail.com>
2024-06-23 21:09:58 +0530

    added error styling on form input and select components

--- components/Forms/Input.js
+++ components/Forms/Input.js
@@ -3,22 +3,30 @@ import Select from "./Select";
 
 function Input({
   label,
+  required,
   placeholder,
   type,
   width,
   value,
   setValue,
   backgroundColor,
+  showError = false,
 }) {
   return (
     <>
       <div className="font-family-bebas flex flex-start items-start flex-col">
-        <label className="text-base">{label}</label>
+        <label className="text-base">
+          {label}
+          <span className="text-red-500">{required && `*`} </span>
+        </label>
         <input
-          className={`border-[1px] border-slate-600  rounded-md px-1 py-2 hover:border-slate-400 ${width} ${backgroundColor}`}
+          className={`border-[1px] ${
+            showError ? `border-red-500` : `border-slate-600`
+          } rounded-md px-1 py-2 hover:border-slate-400 ${width} ${backgroundColor}`}
           type={type}
           placeholder={placeholder}
           value={value}
+          required
           onChange={(e) => setValue(e.target.value)}
         />
       </div>
--- components/Forms/Select.js
+++ components/Forms/Select.js
@@ -10,15 +10,22 @@ function Select({
   displayText,
   values,
   setValue,
+  required,
+  showError = false,
 }) {
   return (
     <>
       <div className="font-family-bebas flex flex-start items-start flex-col">
-        <label className="text-base">{label}</label>
+        <label className="text-base">
+          {label}
+          <span className="text-red-500">{required && `*`}</span>
+        </label>
         <select
           id={id}
           defaultValue={defaultValue ? defaultValue : -1}
-          className={`border-[1px] border-slate-300 px-2 py-[10px] rounded-md cursor-pointer hover:border-indigo-200 ${width} focus:outline-none`}
+          className={`border-[1px] ${
+            showError ? `border-red-500` : `border-slate-600`
+          } px-2 py-[10px] rounded-md cursor-pointer hover:border-indigo-200 ${width} focus:outline-none`}
           onChange={(e) => setValue(e.target.value)}
         >
           {!defaultValue && <option value={-1}>{`${displayText}`}</option>}
